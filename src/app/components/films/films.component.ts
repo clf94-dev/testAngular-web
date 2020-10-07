06ae22ed16eb1ae9fc0cd324d1f4341153c7952e
clf94-dev <clf94@hotmail.es>
2020-10-07 12:39:28 +0200

    update view with DoCheck hook

--- src/app/components/films/films.component.ts
+++ src/app/components/films/films.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-films',
   templateUrl: './films.component.html',
   styleUrls: ['./films.component.scss']
 })
-export class FilmsComponent implements OnInit {
-
-  constructor() { }
+export class FilmsComponent implements OnInit, DoCheck {
+public contador: number;
+  constructor() {this.contador=0; }
 
   ngOnInit(): void {
   }
 
+  ngDoCheck (){
+//Hook que actualiza la vista al ocurrir un cambio
+  }
+
+  buttonClicked() {
+  this.contador++;
+}
 }
