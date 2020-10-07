9640cbcd88058d0adf19b1ff6d7f4de25fff17b2
clf94-dev <clf94@hotmail.es>
2020-10-07 11:57:24 +0200

    created component with command line

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {MiComponent} from './components/mi-component/mi-component.component';
 import {MiComponent2} from './components/mi-component2/mi-component2.component';
+import { FilmsComponent } from './components/films/films.component';
 
 @NgModule({
     declarations: [
-        AppComponent, MiComponent, MiComponent2
+        AppComponent, MiComponent, MiComponent2, FilmsComponent
     ],
     imports: [BrowserModule],
     providers: [],
