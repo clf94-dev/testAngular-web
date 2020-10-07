e8ff95e70281bcf39ee624d3f36961a6f246c215
clf94-dev <clf94@hotmail.es>
2020-10-07 14:03:39 +0200

    example of OnDestroy hook

--- src/app/components/hide-me/hide-me.component.ts
+++ src/app/components/hide-me/hide-me.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-hide-me',
   templateUrl: './hide-me.component.html',
   styleUrls: ['./hide-me.component.scss']
 })
-export class HideMeComponent implements OnInit {
-
-  constructor() { }
+export class HideMeComponent implements OnInit, OnDestroy {
+public showTitle : boolean;
+  constructor() {
+    this.showTitle = true;
+   }
 
   ngOnInit(): void {
   }
 
+  ngOnDestroy(){
+
+  }
+
+  hideClicked(){
+  this.showTitle = false ;
+  }
+  showClicked(){
+    this.showTitle = true;
+  }
 }
